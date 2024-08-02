da09f56721dfd1c5e1e42df70d653f8650255269
21pranshutiwari <pranshupandit241@gmail.com>
2024-08-02 16:07:46 +0530

    Add files via upload

---
+++ navigaton/logic.js
@@ -0,0 +1,34 @@
+const btn =document.querySelector("button");
+const home =document.querySelector("i");
+const search =document.querySelector(".search input");
+const gift =document.querySelector(".offer i");
+const task =document.querySelector(".tasks h3");
+const events =document.querySelector(".event h3");
+const career =document.querySelector(".Career h3");
+const contact =document.querySelector(".contact h3");
+const button =document.querySelector(".dropbtn");
+
+home.onmouseover =() =>{
+    document.body.style.backgroundColor='black';
+} 
+gift.onmouseover =() =>{
+    document.body.style.backgroundColor='blue';
+    
+    } 
+search.onmouseover =() =>{
+    document.body.style.backgroundColor='red';
+    } 
+    task.onmouseover =() =>{
+        document.body.style.backgroundColor='orange';
+    } 
+    events.onmouseover =() =>{
+    document.body.style.backgroundColor='voilet';
+    } 
+    career.onmouseover =() =>{
+        document.body.style.backgroundColor='green';
+        } 
+    contact.onmouseover =() =>{
+        document.body.style.backgroundColor='silver';
+        } 
+        
+            
